574ed139c93becf64f4d3a1eaa7f535018643bdd
Otibra <otienobrian718@gmail.com>
2025-12-07 12:27:49 -0800

    class static method

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,15 @@
+# calculator.py
+class Calculator:
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+    
+    @staticmethod
+    def add(a, b):
+        """Returns the sum of two numbers."""
+        return a + b
+    
+    @classmethod
+    def multiply(cls, a, b):
+        """Prints the class attribute and returns the product of two numbers."""
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
